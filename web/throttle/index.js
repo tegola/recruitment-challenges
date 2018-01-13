0d4817347a7fb15bcc01410c6151e6f64d156104
Alan Sprecacenere <alan@qreate.it>
2018-01-13 15:28:04 +0100

    Throttle challenge

--- web/throttle/index.js
+++ web/throttle/index.js
@@ -1,1 +1,32 @@
-// Your code here
+function throttlePromises(limit, promises) {
+	let index = 0;
+	let results = [];
+	let queue = [];
+
+	const resolveNext = () => {
+		// Stop if we've done all promises
+		if (index >= promises.length) return;
+
+		// Get next promise
+		const currentIndex = index++;
+		const promise = promises[currentIndex];
+
+		// Resolve promise and store result
+		return Promise.resolve(promise())
+			.then(result => {  
+				results[currentIndex] = result;
+				return;
+			}).then(resolveNext);
+	}
+
+	// Add to queue until limit is reached
+	while (index < limit && index < promises.length) {
+		queue.push(resolveNext());
+	}
+
+	return Promise
+		.all(queue)
+		.then(() => results);
+};
+
+module.exports = throttlePromises;

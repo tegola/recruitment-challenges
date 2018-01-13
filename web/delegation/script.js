e79f2726adb16d47a76c9d92facb2f6b356c684a
Alan Sprecacenere <alan@qreate.it>
2018-01-13 15:30:07 +0100

    Delegation challenge

--- web/delegation/script.js
+++ web/delegation/script.js
@@ -1,1 +1,18 @@
-// Your code here
+/**
+ * Notes:
+ * - DOMContentLoaded is not supported on IE8
+ * - Using anonymous functions for brevity
+ */
+
+document.addEventListener('DOMContentLoaded', function() {
+	document
+		.getElementById('items-list')
+		.addEventListener('click', function(event) {
+			const target = event.target;
+
+			if (target.tagName == 'A') {
+				event.preventDefault();
+				alert(target.text);
+			}
+		});
+});
